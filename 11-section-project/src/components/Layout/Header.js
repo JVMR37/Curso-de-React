59898c7e48089e01164ce2723d7084c521512406
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-07-12 11:26:17 -0400

    feat: Finished 11 Section Project

--- 11-section-project/src/components/Layout/Header.js
+++ 11-section-project/src/components/Layout/Header.js
@@ -1,24 +1,21 @@
 import { Fragment } from 'react';
 
-import mealsImage from '../../assets/meals.jpg'
-import classes from './Header.module.css'
-
 import HeaderCartButton from './HeaderCartButton';
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
 
-const Header = props => {
-    return (
-        <Fragment>
-            <header className={classes.header}>
-                <h1>ReactRango</h1>
-                <HeaderCartButton />
-            </header>
-            <div className={classes['main-image']}>
-                <img src={mealsImage}
-                    alt="Uma mesa cheia de comidas deliciosas!"
-                />
-            </div>
-        </Fragment>
-    );
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt='A table full of delicious food!' />
+      </div>
+    </Fragment>
+  );
 };
 
 export default Header;
